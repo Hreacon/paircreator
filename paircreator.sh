cf8045159202e5f0bbce7514053ad02e426ad5ce
Nicholas Jensen-Hay <NicholasJensenHay@gmail.com>
2016-01-05 16:48:40 -0800

    broken update DO NOT USE

--- paircreator.sh
+++ paircreator.sh
@@ -1,4 +1,17 @@
-#!bash/script
+#!/bin/bash
+function writeFile {
+FILE = ~/.pairs
+echo pairs: >> $FILE
+echo "  $initial1: $name1" >> $FILE
+echo "  $initial2: $name2" >> $FILE
+echo email_addresses: >> $FILE
+echo "  $initial1: $email1" >> $FILE
+echo "  $initial2: $email2" >> $FILE
+
+echo $FILE file written.
+}
+
+function collectInput {
 clear
 echo First pairs initials, followed by enter
 read initial1
@@ -18,12 +31,18 @@ clear
 echo Second pairs email, followed by enter
 read email2
 clear
+}
 
-echo pairs: >> ~/.pairs
-echo "  $initial1: $name1" >> ~/.pairs
-echo "  $initial2: $name2" >> ~/.pairs
-echo email_addresses: >> ~/.pairs
-echo "  $initial1: $email1" >> ~/.pairs
-echo "  $initial2: $email2" >> ~/.pairs
-
-echo done
+FILE = ~/.pairs
+if [ -f $FILE ]
+then
+  echo File exists. Overwrite? y/n [case sensitive]
+  read answer
+  if [[ "$answer" == y* ]]
+    then collectInput
+    writeFile
+  else echo Aborted
+  fi
+else collectInput
+  writeFile
+fi
